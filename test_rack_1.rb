436115b77261dc7decd6207e71d7e5cd0876957f
yym-yumeng123 <18026493873@163.com>
2020-07-15 21:28:31 +0800

    feat: rack request

--- test_rack_1.rb
+++ test_rack_1.rb
@@ -5,14 +5,36 @@ require 'webrick'
 
 class RackApp
   def call(env)
-    [
-        # 响应
-        200,
-        {'Content-Type'=> 'text/html;charset=utf-8'},
-        ['<div>你好</div>']
-    ]
+    request = Rack::Request.new(env)
+
+    case request.path
+    when '/index.html'
+      [
+          # 响应
+          200,
+          {'Content-Type'=> 'text/html;charset=utf-8'},
+          ['<link rel="stylesheet" href="style.css"><h1>首页</h1>']
+      ]
+    when '/style.css'
+      [
+          # 响应
+          200,
+          {'Content-Type'=> 'text/css;charset=utf-8'},
+          ['h1{color: red}']
+      ]
+    else
+      [
+          # 响应
+          404,
+          {'Content-Type'=> 'text/html;charset=utf-8'},
+          ['<div>路径不存在</div>']
+      ]
+
+    end
+
   end
 end
 
 # run 后面接受三个参数
+
 Rack::Handler::WEBrick.run RackApp.new, Host: '0.0.0.0', Port: 1234
--- test_rack_2.rb
+++ test_rack_2.rb
@@ -4,7 +4,7 @@ require 'rack'
 require 'webrick'
 
 rackApp = -> (env) {
-  [200, {}, ['Hello']]
+  [200, {'Content-Type'=> 'text/html;charset=utf-8'}, ['Hello']]
 }
 
 Rack::Handler::WEBrick.run rackApp, Host: '0.0.0.0', Port: 1234
