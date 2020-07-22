eafc71563b4f2e2258fcf474409644505179e479
yym-yumeng123 <18026493873@163.com>
2020-07-22 22:08:54 +0800

    feat: 测试 layout render html

--- rails-demo-1/morney-rails-1/app/controllers/first_controller.rb
+++ rails-demo-1/morney-rails-1/app/controllers/first_controller.rb
@@ -1,6 +1,11 @@
 class FirstController < ApplicationController
   def hello
     # render plain: 'hello rails'
-    render json: {name: 'yym', age: 12}
+    # render json: {name: 'yym', age: 12}
+    render 'first/hello'
   end
+  def hi
+    render 'first/hi'
+  end
+
 end
--- rails-demo-1/morney-rails-1/config/routes.rb
+++ rails-demo-1/morney-rails-1/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get (params1: path, params2: object)
   get '/hello', to: 'first#hello'
+  get '/hi', to: 'first#hi'
 end
