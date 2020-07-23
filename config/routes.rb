ac42f50cd49cabb51cc08ce7fdc30f95c4d46fa3
yym-yumeng123 <18026493873@163.com>
2020-07-23 22:52:10 +0800

    users#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   # get (params1: path, params2: object)
   get '/hello', to: 'first#hello'
   get '/hi', to: 'first#hi'
+
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # post 'users', to: 'users#create'
+  # delete 'users/:id', to: 'users#destroy'
+  # patch 'users/:id', to 'users#update'
+
+  # 这句话代替上面的所有语句
+  resources :users
 end
