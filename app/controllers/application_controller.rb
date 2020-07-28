5db7d2f1352da66470340949b6745780f0d4725c
yym-yumeng123 <18026493873@163.com>
2020-07-29 00:01:08 +0800

    极限优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def render_resource(resource)
+    # errors 是 empty
+    if resource.errors.empty?
+      render json: {resource: resource}, status: 200
+    else
+      render json: {errors: resource.errors}, status: 400
+    end
+  end
 end
