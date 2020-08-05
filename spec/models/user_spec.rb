65e07fc27aae28529de638b78958cebd8138644e
yym-yumeng123 <18026493873@163.com>
2020-08-05 21:51:54 +0800

    测试 model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,38 @@ RSpec.describe User, type: :model do
     expect(user.password_digest).to_not eq '123456'
     expect(user.id).to be_a Numeric
   end
+
+  it '可以删除 user' do
+    user = User.create email: '11@qq.com', password: '123456', password_confirmation: '123456'
+    expect {
+      User.destroy_by id: user.id
+    }.to change { User.count }.by(-1)
+  end
+
+  it '创建的时候必须有 email' do
+    user = User.create password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:blank)
+  end
+
+  it '创建时email不能被占用' do
+    # ! 如果不成功立刻报错
+    User.create! email: '11@qq.com', password: '123456', password_confirmation: '123456'
+    user = User.create email: '11@qq.com', password: '123456', password_confirmation: '123456'
+    expect(user.errors.details[:email][0][:error]).to eq(:taken)
+  end
+
+  xit '创建之后发欢迎邮件给用户' do
+    mailer = spy('mailer')
+    allow(UserMailer).to receive(:welcome_email).and_return(mailer)
+    User.create! email: '11@qq.com', password: '123456', password_confirmation: '123456'
+    expect(UserMailer).to have_received(:welcome_email)
+    expect(mailer).to have_received(:deliver_later)
+  end
+
+  it '邮箱为空字符串则只提示邮箱为空' do
+    user = User.create email: ''
+    p user.errors.details[:email]
+    expect(user.errors.details[:email].length).to eq 2
+    expect(user.errors.details[:email][0][:error]).to eq :blank
+  end
 end
