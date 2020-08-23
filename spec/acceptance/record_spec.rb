70690d5d048960441585da73cdc42bc1977ff77f
yym-yumeng123 <18026493873@163.com>
2020-08-23 21:51:20 +0800

    获取 records & record 的文档

--- spec/acceptance/record_spec.rb
+++ spec/acceptance/record_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Records" do
+  let(:record){Record.create! amount: 10000, category: 'income'}
+  let(:id){record.id}
+
   post "/records" do
     parameter :amount, '金额', type: :integer, required: true
     parameter :category, '类型: outgoings: 1|income: 2', type: :integer, required: true
@@ -20,9 +23,29 @@ resource "Records" do
   end
 
   delete "/records/:id" do
-    let(:record){Record.create! amount: 10000, category: 'income'}
-    let(:id){record.id}
     example "删除记录" do
+      sign_in
+      do_request
+      expect(status).to eq 200
+    end
+  end
+
+  get "/records" do
+    parameter :page, '页码', type: :integer
+    (1..11).to_a.map do
+      Record.create! amount: 10000, category: 'income'
+    end
+    let(:page) {1}
+    example '获取所有记录' do
+      sign_in
+      do_request
+      expect(status).to eq 200
+    end
+
+  end
+
+  get "/records/:id" do
+    example '获取一个的记录' do
       sign_in
       do_request
       expect(status).to eq 200
