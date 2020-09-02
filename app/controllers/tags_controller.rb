e53743dbfebcb3ca411ae18a06ff8fd7e725e19a
yym-yumeng123 <18026493873@163.com>
2020-09-02 12:01:54 +0800

    index and show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,24 @@
 class TagsController < ApplicationController
   before_action :must_sign_in
 
+  # 新建
   def create
     render_resource Tag.create create_params
   end
 
+  # 删除
   def destroy
     tag = Tag.find(params[:id])
     head tag.destroy ? :ok : :bad_request
+  end
+
+  # 查询所有
+  def index
+    render_resources Tag.page(params[:page])
+  end
+
+  def show
+    render_resource Tag.find(params[:id])
   end
 
 
